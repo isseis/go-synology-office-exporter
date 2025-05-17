1d2f5472d6ee49c1f5c6c13acc918b80891dd02e
Issei Suzuki <4515431+isseis@users.noreply.github.com>
2025-05-17 21:18:56 +0900

    test: use go:embed for canned API responses and move JSON to data files
    - Replace large inline cannedResponseListFiles and cannedResponseGetFile constants with files embedded using go:embed
    - Add new data files: data/files_list_response.json and data/files_get_response.json for mock API responses
    - Update mockSynologyHandler to write embedded []byte directly for list and get responses

--- synology_drive_api/api_test.go
+++ synology_drive_api/api_test.go
@@ -1,6 +1,7 @@
 package synology_drive_api
 
 import (
+	_ "embed"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -34,207 +35,11 @@ func ResetMockLogin() {
 	mockLoggedIn = false
 }
 
-const cannedResponseListFiles = `
-{
-	"data": {
-		"items": [
-			{
-				"access_time": 1746239241,
-				"adv_shared": false,
-				"app_properties": {
-					"type": "none"
-				},
-				"capabilities": {
-					"can_comment": true,
-					"can_delete": true,
-					"can_download": true,
-					"can_encrypt": true,
-					"can_organize": true,
-					"can_preview": true,
-					"can_read": true,
-					"can_rename": true,
-					"can_share": true,
-					"can_sync": true,
-					"can_write": true
-				},
-				"change_id": 17,
-				"change_time": 1746239241,
-				"content_snippet": "",
-				"content_type": "dir",
-				"created_time": 1746239202,
-				"disable_download": false,
-				"display_path": "/mydrive/2025-04",
-				"dsm_path": "",
-				"enable_watermark": false,
-				"encrypted": false,
-				"file_id": "882614106016756317",
-				"force_watermark_download": false,
-				"hash": "",
-				"image_metadata": {
-					"time": 1746239241
-				},
-					"labels": [],
-				"max_id": 14,
-				"modified_time": 1746239241,
-				"name": "2025-04",
-				"owner": {
-					"display_name": "backup",
-					"name": "backup",
-					"nickname": "",
-					"uid": 1029
-				},
-				"parent_id": "880423525918227781",
-				"path": "/2025-04",
-				"permanent_link": "13CNgI0QlJMjbqcZf5k3HRoZsbCAqejY",
-				"properties": {},
-				"removed": false,
-				"revisions": 1,
-				"shared": false,
-				"shared_with": [],
-				"size": 0,
-				"starred": false,
-				"support_remote": false,
-				"sync_id": 8,
-				"sync_to_device": false,
-				"transient": false,
-				"type": "dir",
-				"version_id": "8",
-				"watermark_version": 0
-			},
-			{
-				"access_time": 1746239530,
-				"adv_shared": false,
-				"app_properties": {
-					"type": "none"
-				},
-				"capabilities": {
-					"can_comment": true,
-					"can_delete": true,
-					"can_download": true,
-					"can_encrypt": true,
-					"can_organize": true,
-					"can_preview": true,
-					"can_read": true,
-					"can_rename": true,
-					"can_share": true,
-					"can_sync": true,
-					"can_write": true
-				},
-				"change_id": 17,
-				"change_time": 1746239530,
-				"content_snippet": "",
-				"content_type": "document",
-				"created_time": 1746239207,
-				"disable_download": false,
-				"display_path": "/mydrive/planning.osheet",
-				"dsm_path": "",
-				"enable_watermark": false,
-				"encrypted": false,
-				"file_id": "882614125167948399",
-				"force_watermark_download": false,
-				"hash": "7dd71dd1192ca985884d934470fb9d3c",
-				"image_metadata": {
-					"time": 1746239207
-				},
-				"labels": [],
-				"max_id": 17,
-				"modified_time": 1746239207,
-				"name": "planning.osheet",
-				"owner": {
-					"display_name": "backup",
-					"name": "backup",
-					"nickname": "",
-					"uid": 1029
-				},
-				"parent_id": "880423525918227781",
-				"path": "/planning.osheet",
-				"permanent_link": "13CNgcuVDEENCkSfDkcKpr39pcK6hhPD",
-				"properties": {
-					"object_id": "1029_ELSD3CRAC557FDOVULJJJDKN50.sh"
-				},
-				"removed": false,
-				"revisions": 3,
-				"shared": false,
-				"shared_with": [],
-				"size": 720,
-				"starred": false,
-				"support_remote": false,
-				"sync_id": 17,
-				"sync_to_device": false,
-				"transient": false,
-				"type": "file",
-				"version_id": "17",
-				"watermark_version": 0
-			}
-		],
-		"total": 2
-	},
-	"success": true
-}`
+//go:embed data/files_list_response.json
+var cannedResponseListFiles []byte
 
-const cannedResponseGetFile = `
-{
-	"data": {
-		"access_time": 1746357311,
-		"adv_shared": false,
-		"app_properties": {"type": "none"},
-		"capabilities": {
-			"can_comment": true,
-			"can_delete": true,
-			"can_download": true,
-			"can_encrypt": true,
-			"can_organize": true,
-			"can_preview": true,
-			"can_read": true,
-			"can_rename": true,
-			"can_share": true,
-			"can_sync": true,
-			"can_write": true
-		},
-		"change_id": 17,
-		"change_time": 1746239530,
-		"content_snippet": "",
-		"content_type": "document",
-		"created_time": 1746239207,
-		"disable_download": false,
-		"display_path": "/mydrive/planning.osheet",
-		"dsm_path": "",
-		"enable_watermark": false,
-		"encrypted": false,
-		"file_id": "882614125167948399",
-		"force_watermark_download": false,
-		"hash": "7dd71dd1192ca985884d934470fb9d3c",
-		"image_metadata": {"time": 1746239207},
-		"labels": [],
-		"max_id": 17,
-		"modified_time": 1746239207,
-		"name": "planning.osheet",
-		"owner": {
-			"display_name": "backup",
-			"name": "backup",
-			"nickname": "",
-			"uid": 1029
-		},
-		"parent_id": "880423525918227781",
-		"path": "/planning.osheet",
-		"permanent_link": "13CNgcuVDEENCkSfDkcKpr39pcK6hhPD",
-		"properties": {"object_id": "1029_ELSD3CRAC557FDOVULJJJDKN50.sh"},
-		"removed": false,
-		"revisions": 3,
-		"shared": false,
-		"shared_with": [],
-		"size": 720,
-		"starred": false,
-		"support_remote": false,
-		"sync_id": 17,
-		"sync_to_device": false,
-		"transient": false,
-		"type": "file",
-		"version_id": "17",
-		"watermark_version": 0
-	},
-	"success": true
-}`
+//go:embed data/files_get_response.json
+var cannedResponseGetFile []byte
 
 func mockSynologyHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("[MOCK] %s %s\n", r.Method, r.URL.String())
@@ -266,11 +71,11 @@ func mockSynologyHandler(w http.ResponseWriter, r *http.Request) {
 		}
 
 		if api == APINameSynologyDriveFiles && method == "list" {
-			w.Write([]byte(cannedResponseListFiles))
+			w.Write(cannedResponseListFiles)
 			return
 		} else if method == "get" {
 			w.Header().Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
-			w.Write([]byte(cannedResponseGetFile))
+			w.Write(cannedResponseGetFile)
 			return
 		} else {
 			resp := map[string]any{
